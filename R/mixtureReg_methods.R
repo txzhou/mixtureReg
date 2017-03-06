637ab01812e00fd3cfb8a5c42fd911dbff7808e1
Tianxia Zhou <tianxiazhou@gmail.com>
2017-03-05 18:12:38 -0800

    update "plot" method. add "logLik" method.

--- R/mixtureReg_methods.R
+++ R/mixtureReg_methods.R
@@ -5,27 +5,36 @@ orderedLines <- function(x, y, ...) {
   lines(x = x[xOrder], y = y[xOrder], ...)
 }
 
-plot.mixtureReg <- function(mixtureModel, yName = NULL, xName = NULL, which = 1:2) {
+plot.mixtureReg <- function(mixtureModel, which = 1:2,
+                            yName = NULL, xName = NULL,
+                            xlab = NULL, ylab = NULL,
+                            ...) {
   # plot method for "mixtureReg" class
 
   if (is.null(yName)) {yName = all.vars(mixtureModel$lmList[[1]]$terms)[1]}
   if (is.null(xName)) {xName = all.vars(mixtureModel$lmList[[1]]$terms)[2]}
+  if (is.null(xlab)) {xlab = xName}
+  if (is.null(ylab)) {ylab = yName}
 
   XX = mixtureModel$regData[ , xName]
   YY = mixtureModel$regData[ , yName]
   YhatList = lapply(X = mixtureModel$lmList, FUN = function(x) predict(x))
 
-  if (1 %in% which) {
-    plot(x = XX, y = YY, xlab = xName, ylab = yName)
+  if (which == 1) {
+    plot(x = XX, y = YY, xlab = xlab, ylab = ylab, ...)
     for (i in 1:length(mixtureModel$lmList)) {
       orderedLines(x = XX, y = YhatList[[i]], col = i + 1)
     }
   }
 
-  if (2 %in% which) {
+  if (which == 2) {
     for (i in 1:length(mixtureModel$lmList)) {
-      plot(x = XX, y = mixtureModel$posterior[[i]], xlab = xName, ylab = paste0("Weights_", i))
+      plot(x = XX, y = mixtureModel$posterior[[i]], xlab = xlab, ylab = paste0("Weights_", i), ...)
       orderedLines(x = XX, y = mixtureModel$prior[[i]], col = i + 1)
     }
   }
+}
+
+logLik.mixtureReg <- function(mixtureModel) {
+  return(mixtureModel$"logLik")
 }
